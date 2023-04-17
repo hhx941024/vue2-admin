27ef04f045f415c61a566487febea4952513acdc
LAPTOP-9E6RKMIA\卖鲍小郎君 <768048745@qq.com>
2023-04-17 17:33:05 +0800

    添加elementui,axios,mock

--- vue2-admin/src/main.js
+++ vue2-admin/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import MOCK from "./mock/index"
+import axios from 'axios'//引入axios
+Vue.prototype.$axios = axios;//把axios挂载到vue上
 Vue.config.productionTip = false
-
+Vue.use(ElementUI);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
